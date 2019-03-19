fa4afa7ad9d58974e4bb7f4cbb76e75aeeefcc23
boruch d <dubywider@gmail.com>
2019-03-19 17:27:02 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(int)
-  if int % 3 == 0 and int % 5 == 0
+  if int / 3 == 0 and int % 5 == 0
     return "FizzBuzz"
-  elsif int % 5 == 0
+  elsif int / 5 == 0
     return "Buzz"
-  elsif int % 3 == 0
+  elsif int / 3 == 0
     return  "Fizz"
 
   end
