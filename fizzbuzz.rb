784d0a73c3fbc682a1ab6dd710c0901694dc272d
boruch d <dubywider@gmail.com>
2019-03-19 16:57:53 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,13 +1,23 @@
-equire_relative './spec_helper.rb'
+def fizzbuzz(int)
+  if int % 3 == 0 # if the number int is divisible by 3
+     int = "Fizz" # Go fizz
 
- define "fizzbuzz".to i
-  if i % 3 == i % 5 == 0
-    "fizzbuzz"
-  elsif
-    i % 3 == 0
-    "fizz"
-  elsif
-    i % 5 == 0
-    "buzz"
 
-   end  
+  elsif int % 5 == 0 
+   int ="Buzz"
+
+
+  elsif int % 15 == 0
+# int = "FizzBuzz"
+
+
+  else 
+#
+
+ # end
+
+
+     # def fizzbuzz(int)
+    #   if int % 3 == 0 || int % 5 == 0# if the number int is divisible by 3
+    #     "FizzBuzz" # Go fizz
+    #   end
