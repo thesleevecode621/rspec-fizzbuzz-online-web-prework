47733fb6cdd76d5d841093d4b00af9f0d33abd28
boruch d <dubywider@gmail.com>
2019-03-19 16:56:45 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
-# Don't forget! This file needs to be 'required' in its spec file
-# See README.md for instructions on how to do this
+equire_relative './spec_helper.rb'
+
+ define "fizzbuzz".to i
+  if i % 3 == i % 5 == 0
+    "fizzbuzz"
+  elsif
+    i % 3 == 0
+    "fizz"
+  elsif
+    i % 5 == 0
+    "buzz"
+
+   end  
