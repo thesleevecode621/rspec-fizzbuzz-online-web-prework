a3e226f42a9fa41c5b7d322723a04f4eb9bb7b9c
boruch d <dubywider@gmail.com>
2019-03-19 16:59:51 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,23 +1,24 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3
-     int = "Fizz" # Go fizz
+      int = "Fizz" # Go fizz
 
 
-  elsif int % 5 == 0 
+ elsif int % 5 == 0 
    int ="Buzz"
 
 
   elsif int % 15 == 0
-# int = "FizzBuzz"
+  int = "FizzBuzz"
 
 
   else 
-#
-
- # end
+ puts "not divisible by 3 or 5"
 
 
-     # def fizzbuzz(int)
-    #   if int % 3 == 0 || int % 5 == 0# if the number int is divisible by 3
-    #     "FizzBuzz" # Go fizz
-    #   end
+  end
+
+
+     def fizzbuzz(int)
+       if int % 3 == 0 || int % 5 == 0# if the number int is divisible by 3
+         "FizzBuzz" # Go fizz
+       end
